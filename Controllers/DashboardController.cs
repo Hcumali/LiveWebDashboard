2d968f073155ced1f509901f0b47d724495ce727
Huseyin Cumali <huseyin@kiwilabs.co>
2020-10-06 15:34:28 +0300

    Last Changes Before Database process

---
+++ Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deneme1.Controllers
+{
+    public class DashboardController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            HttpCookie cookie_islogged = Request.Cookies["isLoggedIn"];
+            if(cookie_islogged == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            if(cookie_islogged.Value != "True")
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return View();
+        }
+
+        public ActionResult SignOut()
+        {
+            return View();
+        }
+    }
+}
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,28 +8,60 @@ namespace deneme1.Controllers
 {
     public class HomeController : Controller
     {
+        
+
         public ActionResult Index()
         {
             return View();
         }
 
+
         public ActionResult SignIn()
         {
             return View();
         }
-
         public ActionResult SignUp()
         {
             return View();
         }
 
-        /*public void LoginOperation(string userName, string password)
+        [HttpPost]
+        public Boolean LoginProcess(string userNameLogin, string passwordLogin)
         {
-            // If login operation is succesfull
-            HttpCookie myCookie = new HttpCookie("isLoggedIn");
-            myCookie.Value = "false";
-            myCookie.Expires = DateTime.Now.AddDays(1d);
-            Response.Cookies.Add(myCookie);
-        }*/
+            if(userNameLogin=="admin" && passwordLogin == "1234")
+            {
+                HttpCookie myCookie = new HttpCookie("isLoggedIn");
+                myCookie.Value = "True";
+                myCookie.Expires = DateTime.Now.AddDays(1);
+                Response.Cookies.Add(myCookie);
+
+                return true;
+        
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        [HttpPost]
+        public Boolean RegisterProcess(string userNameRegister, string passwordRegister, int age)
+        {
+            if (userNameRegister != "yyyyy000" && passwordRegister != "12345678" && age >= 18)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+
+
+
+
     }
 }
