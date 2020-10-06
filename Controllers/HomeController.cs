802016589b3586dde7f2647d1f8add868e43d82e
Huseyin Cumali <huseyin@kiwilabs.co>
2020-10-06 08:52:29 +0300

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deneme1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        /*public void LoginOperation(string userName, string password)
+        {
+            // If login operation is succesfull
+            HttpCookie myCookie = new HttpCookie("isLoggedIn");
+            myCookie.Value = "false";
+            myCookie.Expires = DateTime.Now.AddDays(1d);
+            Response.Cookies.Add(myCookie);
+        }*/
+    }
+}
